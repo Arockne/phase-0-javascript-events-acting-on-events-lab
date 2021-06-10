572c2aaec01e739170bd2463454e83ff4eadd391
Daniel Calvin <arockne2003@yahoo.com>
2021-06-10 08:59:34 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft() {
+  const dodgerLocation = parseInt(dodger.style.left.replace('px', ''), 10);
+  if (dodgerLocation >= 0) {
+    dodger.style.left = `${dodgerLocation - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const dodgerLocation = parseInt(dodger.style.left.replace('px', ''), 10);
+  if (dodgerLocation <= 360) {
+    dodger.style.left = `${dodgerLocation + 1}px`;
+  }
+}
+document.body.addEventListener('keydown', function move(event) {
+  if (event.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  }
+  if (event.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+})
